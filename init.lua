5d2b7e22856a6e55206ac21ad37fbc1ffaf2d061
David Lamb <david.lamb@grow.inc>
2026-05-03 13:43:56 +1000

    skip kickstart config when running inside VS Code

--- init.lua
+++ init.lua
@@ -84,6 +84,11 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- Skip kickstart config when running inside VS Code
+if vim.g.vscode then
+  return
+end
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
